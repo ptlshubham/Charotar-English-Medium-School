dc92c09cc89ed68245df92c923fa51a30ee16a50
Shubham Patel <ptlshubham@gmail.com>
2023-09-26 12:14:15 +0530

    all changes

--- src/app/more/more.module.ts
+++ src/app/more/more.module.ts
@@ -11,6 +11,10 @@ import { NewsComponent } from './news/news.component';
 import { SyllabusComponent } from './syllabus/syllabus.component';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { MagazineComponent } from './magazine/magazine.component';
+import { PaperComponent } from './paper/paper.component';
+import { ResultComponent } from './result/result.component';
+import { ScholarshipComponent } from './scholarship/scholarship.component';
+import { AcademicCalendarComponent } from './academic-calendar/academic-calendar.component';
 
 
 
@@ -21,7 +25,11 @@ import { MagazineComponent } from './magazine/magazine.component';
     EvalutionComponent,
     NewsComponent,
     SyllabusComponent,
-    MagazineComponent
+    MagazineComponent,
+    PaperComponent,
+    ResultComponent,
+    ScholarshipComponent,
+    AcademicCalendarComponent
   ],
   imports: [
     CommonModule,
--- src/app/more/more.routing.ts
+++ src/app/more/more.routing.ts
@@ -5,6 +5,10 @@ import { MagazineComponent } from './magazine/magazine.component';
 import { NewsComponent } from './news/news.component';
 import { SearchComponent } from './search/search.component';
 import { SyllabusComponent } from './syllabus/syllabus.component';
+import { PaperComponent } from './paper/paper.component';
+import { ResultComponent } from './result/result.component';
+import { ScholarshipComponent } from './scholarship/scholarship.component';
+import { AcademicCalendarComponent } from './academic-calendar/academic-calendar.component';
 export const MoreRoutes: Routes = [{
     path: '',
     children: [
@@ -31,6 +35,22 @@ export const MoreRoutes: Routes = [{
         {
             path: 'magazine',
             component: MagazineComponent
+        },
+        {
+            path: 'paper',
+            component: PaperComponent
+        },
+        {
+            path: 'result',
+            component: ResultComponent
+        },
+        {
+            path:'scholarship',
+            component:ScholarshipComponent
+        },
+        {
+            path: 'academic-calendar',
+            component: AcademicCalendarComponent
         }
     ]
 }];
