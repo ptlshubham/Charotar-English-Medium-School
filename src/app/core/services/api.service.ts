16f0618c9dc8d36264ec7bb58aa56d396af7665e
Shubham Patel <ptlshubham@gmail.com>
2023-10-02 11:08:35 +0530

    server backend link chnage

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -6,7 +6,7 @@ declare var $: any;
   providedIn: 'root'
 })
 export class ApiService {
-  public static HOST_URL: string = "https://bapsanandmandir.co.in";
+  public static HOST_URL: string = "https://api.cesociety.in";
   // public static HOST_URL: string = "http://localhost:9000";
   toasts: any[] = [];
   constructor(
